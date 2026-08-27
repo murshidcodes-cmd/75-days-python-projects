aa2a8beadff5a260dccde068d382b367e91abec6
Murshid Codes 🐍 <astheticnature12@gmail.com>
2026-08-28 03:16:26 +0530

    Add Day 14 project

---
+++ Day-14-Digital-Clock/main.py
@@ -0,0 +1,13 @@
+"""
+DAY 14/75
+PYTHON PROJECT JOURNEY
+"""
+
+import time
+
+while True:
+    current_time = time.strftime("%H:%M:%S")
+
+    print(f"\r🕐 {current_time}", end="")
+
+    time.sleep(1)
