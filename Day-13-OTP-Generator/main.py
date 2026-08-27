cc66bacbcab773d253ddee84641e007168a468e0
Murshid Codes 🐍 <astheticnature12@gmail.com>
2026-08-28 03:07:32 +0530

    Add Day 13 project

---
+++ Day-13-OTP-Generator/main.py
@@ -0,0 +1,13 @@
+"""
+DAY 13/75
+PYTHON PROJECT JOURNEY
+"""
+
+import random
+
+print("🔐 OTP GENERATOR")
+
+otp = random.randint(100000, 999999)
+
+print("\nGenerating OTP...")
+print(f"Your OTP: {otp}")
